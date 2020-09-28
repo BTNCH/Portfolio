29bf3373f57fb274f53e295fc2af9435da726d30
Tanachart <614259044@webail.npru.ac.th>
2020-09-28 14:40:44 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,12 @@
+const data = {
+    "name":"Tanachart Chaonafai",
+    "work":"Studen",
+    "aboutme":{
+        "Full name":"Tanachart Chaonafai",
+        "Phone":"(+66)090 445 0984",
+        "Email":"614259044@webmail.npru.ac.th",
+        "Website":"http://facebook/Tanachad.com",
+        "Address":" 29/2 Moo 6, Wang Taku Subdistrict, Nakhon Pathom Province 73000"
+    }
+}
+module.exports = data;
---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const myData = require('../data');
+
+
+router.get('/',(req,res)=>{
+    res.render('index',{myData});
+
+
+});
+router.get('/aboutme',(req,res)=>{
+    res.render('about',{myData});
+
+});
+router.get('/education',(req,res)=>{
+    res.send("<h1> Education </h1>");
+
+});
+router.get('/workexperience',(req,res)=>{
+    res.send("<h1>My work Experience</h1>");
+
+});
+router.get('/skill',(req,res)=>{
+    res.send("<h1>My Skill</h1>");
+
+});
+
+module.exports = router;
